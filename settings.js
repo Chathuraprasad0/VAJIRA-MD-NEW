fb1c88c0ba22543ce2ea0b0aeb258a5f7397838a
Chathuraprasad0 <165113680+Chathuraprasad0@users.noreply.github.com>
2024-09-05 20:26:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94704051516' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=vMVEjBgQ#oIMe0Td-hYkZkTBE236uEf-hoDrBpi6CCzRSHqRta-Q' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=rQEXGZIJ#G4NDhT34OLtlfajXbd_loelFfyKkzrWKWCtr9aYIdE4' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94704051516" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
